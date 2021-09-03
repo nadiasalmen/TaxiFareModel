528ab3f3379cf0a45196bde4b86cd16ebfb5bc37
Nadia Salmen <nadiasalmen@gmail.com>
2021-09-03 09:33:11 -0300

    Add ml_flow_test

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,8 +8,15 @@ from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from sklearn.model_selection import train_test_split
 import joblib
 from TaxiFareModel.utils import compute_rmse
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
 class Trainer():
+
+    EXPERIMENT_NAME = "[AR][Buenos Aires][nadiasalmen] taxifare v3"
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -49,20 +56,61 @@ class Trainer():
         """set and train the pipeline"""
         self.pipeline = self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
+        self.mlflow_log_param("model", "linear")
         # joblib.dump(self.pipeline, 'model.joblib')
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
         return rmse
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
 
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.EXPERIMENT_NAME).experiment_id
+
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+print(__name__)
 if __name__ == "__main__":
     # get data
     # clean data
     # set X and y
     # hold out
     # train
-    # evaluate
     print('TODO')
+    from TaxiFareModel.data import get_data, clean_data
+
+    N = 100
+    df = get_data(nrows=N)
+    df = clean_data(df)
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
+    trainer.evaluate(X_test, y_test)
